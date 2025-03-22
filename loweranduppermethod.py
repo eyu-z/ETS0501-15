f76c9541cf4f242ea2f56f57caad854914e3a882
eyu-z <eyoelzeleke49@gmail.com>
2025-03-22 20:38:51 +0200

    ETS0501/15

---
+++ loweranduppermethod.py
@@ -0,0 +1,39 @@
+#lowercase 
+
+text = "HeRE IS OUR first text and IT is written in BOTH capital anD SMAll."
+lowercase_text = text.lower()
+
+print(f"the text is :{text}")
+print(f"the changed lowercase text is :{lowercase_text}")
+#this changes all the characters becuase all the characters are strings.
+
+#example 2
+
+text_2 = "THIS is THe 2nd teXT with some numBERS LIke 1,2 anD 3."
+lowercase_text_2 = text_2.lower()
+
+
+print(f"text_2 is : {text_2}")
+print(f"text_2 has changed to lowercase : {lowercase_text_2}")
+#this one changes the string part and leave the numbers as it is.
+
+
+#uppercase
+
+text_3 = "there IS SOME LOWERCAses that must be chANGED TO uppercase."
+uppercase_text_3 = text_3.upper()
+
+print(f"the text is : {text_3}")
+print(f"the edited uppercase text is: {uppercase_text_3}")
+
+#all the lowercase stings are changed to uppercase.
+ 
+#example 2
+
+text_4 = "this ONE includES A numbers and specail characters liKE 1,2,@,$ AND 8."
+uppercase_text_4 = text_4.upper()
+
+print(f"the text is: {text_4}")
+print(f"the edited text is: {uppercase_text_4}")
+
+#all lowercase strings have changed but the numbers and those some special characters are till unchanged.
